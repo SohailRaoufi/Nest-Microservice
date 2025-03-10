c6660385f221c42f9c01f7678e2817957be62f82
Sohail Raoufi <sohailraufi19@gmail.com>
2025-03-10 21:38:37 +0430

    init

--- apps/category-service-e2e/jest.config.ts
+++ apps/category-service-e2e/jest.config.ts
@@ -1,5 +1,5 @@
 export default {
-  displayName: 'nx-microservices-e2e',
+  displayName: 'category-service-e2e',
   preset: '../../jest.preset.js',
   globalSetup: '<rootDir>/src/support/global-setup.ts',
   globalTeardown: '<rootDir>/src/support/global-teardown.ts',
@@ -14,5 +14,5 @@ export default {
     ],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/nx-microservices-e2e',
+  coverageDirectory: '../../coverage/category-service-e2e',
 };
